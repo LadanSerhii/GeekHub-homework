80c9b7b2005506b1ea14e99fd5d04f563263b8a9
Serhii Ladan <sergeyladan@gmail.com>
2022-10-06 12:14:19 +0300

    Add new file

---
+++ HT_1/task_1.py
@@ -0,0 +1,5 @@
+# Python 3 - installed
+# PIP - installed
+# Sublime Text - installed
+#
+# At the present time on Windows, becouse of some problems with hardware. Hope to solve it in few lessons and continue with Linux.
