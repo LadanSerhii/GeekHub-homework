b4766be58314d69cc93701ff1dbdaed149f8da0d
Serhii Ladan <sergeyladan@gmail.com>
2022-10-06 12:44:10 +0300

    Add file #5

---
+++ HT_1/task_5.py
@@ -0,0 +1,16 @@
+# Types of data in Python:
+#
+# Numbers			
+# Strings - sequence of symbols
+# Lists - could be different type of data - list_of_values = ['Anna', 2.6, 13]
+# Dictionaries - sets of key-value pairs - dict_of_values = {1:'value', 'key':2}
+# Tuples - like lists, but unchangeble - tuple_of_values = (1+4j, 5.2, "Leon")
+# Sets - unordered unique values - set_of_values = {1, 3, 4, 56, 23, 12}
+# Booleans
+
+# Unchangeble: numbers, strings, tuples
+# Changable: lists, dictionaries, sets
+# Ordered: lists, tuples, string, dictionaries
+# Unordered: sets
+
+# 
