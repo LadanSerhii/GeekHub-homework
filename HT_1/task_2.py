637cefcb20aa878e0de57bbe4d0dc65dc4b04178
Serhii Ladan <sergeyladan@gmail.com>
2022-10-06 11:17:20 +0300

    Second commit

--- HT_1/task_2.py
+++ HT_1/task_2.py
@@ -7,3 +7,6 @@
 # git diff - show the changes that not yet staged
 # git reset
 # git status - lists files that had to be commited
+# git rm
+# git log - list the version history of current branch
+# git show - shows the metadata and content changes of the cpecified commit
