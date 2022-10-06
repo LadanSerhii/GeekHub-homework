3bab121f618a7638e4ad7ae06dba29692dca2eaa
Serhii Ladan <sergeyladan@gmail.com>
2022-10-06 11:34:21 +0300

    4th iteration

--- HT_1/task_2.py
+++ HT_1/task_2.py
@@ -12,3 +12,8 @@
 # git show - shows the metadata and content changes of the cpecified commit
 # git branch - lists the local branches in current repository
 # git push - send the commited changes of master branch to your remote repository
+# git pull - fetches and merge changes on the remote server to your working directory
+# git stash - temporary stores all the modified tracked files
+# git show - show the list of changes (hash)
+# git revert - cancel of last commit
+# git remote add 'shortname' URL - add remote repository
