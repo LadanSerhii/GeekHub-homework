c6eaf053aba0f451e5e947ceaa1104d2b3268dec
LadanSerhii <113498877+LadanSerhii@users.noreply.github.com>
2022-10-12 22:53:36 +0300

    HT_2

--- HT_2/task_1.py
+++ HT_2/task_1.py
@@ -1,11 +1,7 @@
 # Write a script which accepts a sequence of comma-separated numbers 
 # from user and generates a list and a tuple with those numbers
-#
-# Version 2: as listed in the statement the values are numbers, so
-# list or a tuple is with number (integer in this case) values
 
-
-string_1 = input()
+string_1 = input('Please eneter numbers: ')
 list_1 = (string_1.split(','))
 for i in range(len(list_1)):
 	if list_1[i].isnumeric():
