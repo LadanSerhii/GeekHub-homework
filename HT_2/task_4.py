b482873c979004f98bc7a873b78bcbe4c6d71d64
LadanSerhii <113498877+LadanSerhii@users.noreply.github.com>
2022-10-12 22:56:07 +0300

    HT_2

--- HT_2/task_4.py
+++ HT_2/task_4.py
@@ -2,9 +2,8 @@
 # times asks user for string input. At the end script must print 
 # out result of concatenating all <number> strings.
 
-number = int(input())
+number = int(input('Please eneter number of strings: '))
 total_str = ''
 for i in range(number):
-	total_str += input()
-print(total_str)
-	
+	total_str += input(f'Please enter string #{i + 1}: ')
+print(f'Result string is: {total_str}')
