dec4bdcbeef10eabd01cb3fd972c9fba3ab9941b
LadanSerhii <113498877+LadanSerhii@users.noreply.github.com>
2022-10-12 22:55:36 +0300

    HT_2

--- HT_2/task_3.py
+++ HT_2/task_3.py
@@ -1,9 +1,8 @@
 # Write a script which accepts a <number> from user and print out 
 # a sum of the first <number> positive integers.
 
-number = int(input())
+number = int(input('Please enter a number: '))
 total = 0
 for i in range(number + 1):
 	total += i
 print(total)
-	
