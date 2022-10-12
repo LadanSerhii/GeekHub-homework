ab3ca19d3b67fe4bdb26887dc7dcbd52a04945a3
LadanSerhii <113498877+LadanSerhii@users.noreply.github.com>
2022-10-12 22:57:08 +0300

    HT_2

--- HT_2/task_5.py
+++ HT_2/task_5.py
@@ -1,7 +1,5 @@
 # Write a script which accepts a decimal number from user and 
 # converts it to hexadecimal
 
-st = input()
-list_1 = [1, 2, 'u', 'a', 4, True]
-print(st in list_1)
-	
+st = int(input('Please enter a decimal number: '))
+print(f'The hexadecimal convertion of your number: {hex(st)}')
