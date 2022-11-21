2a049dacdbcc457953c7d373d4f1534df354c85c
Serhii Ladan <sergeyladan@gmail.com>
2022-11-21 17:40:06 +0200

    HT_12 rev.1

--- HT_12/task_1.py
+++ HT_12/task_1.py
@@ -10,17 +10,24 @@ class Figure(object):
 
 
 class Oval(Figure):
+
     def __init__(self, dim_1, dim_2):
         print(f'This is {self.color} oval with {dim_1} and {dim_2} dimensions!' )
+        self.dim_1 = dim_1
+        self.dim_2 = dim_2
 
 
 class Square(Figure):
     def __init__(self, side):
         print(f'This is {self.color} square with {side} dimension!')
+        self.side = side
 
 
 ov = Oval(7, 167)
+print(ov.dim_1)
+print(ov.dim_2)
 sq = Square(260)
+print(sq.side)
 
 ov.change_color('red')
 sq.change_color('green')
