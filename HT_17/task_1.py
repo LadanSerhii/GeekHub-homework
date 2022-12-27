1e090771302c77ebb0075fc20fb00a798328b3f5
Serhii Ladan <sergeyladan@gmail.com>
2022-12-27 13:21:19 +0200

    HT_17 rev. 2

--- HT_17/task_1.py
+++ HT_17/task_1.py
@@ -3,12 +3,11 @@ import csv
 import codecs
 import shutil
 from urllib.request import urlopen
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
